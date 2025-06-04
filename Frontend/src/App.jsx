ab98692b871badc463d611e988595d00b1f2ee40
karthik s r <karthik.s.r@gds.ey.com>
2025-06-04 13:50:54 +0530

    added new section in the main layout for the app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UserLayout from "./components/Layout/UserLayout";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import UserLayout from './components/Layout/UserLayout'
+import Home from './pages/Home'
 
-export default function App() {
+export default function App () {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<UserLayout />} />
+        <Route path='/' element={<UserLayout />}>
+          <Route index element={<Home />} />
+        </Route>
         {/* <Route path="about" element={<About />} /> */}
       </Routes>
     </BrowserRouter>
